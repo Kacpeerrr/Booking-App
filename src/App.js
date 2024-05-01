f7ad050fadb93dcde17b2efb8f838f4be44051d6
Kacper <kacpeeer.mikolajczyk@gmail.com>
2024-05-01 22:28:16 +0200

    added logic for searchbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,70 @@
+import { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header'
 import Menu from './components/Menu/Menu'
 import Hotels from './components/Hotels/Hotels';
+import LoadingIcon from './components/UI/LoadingIcon/LoadingIcon';
+
+class App extends Component {
+    
+    hotels = [
+      {
+        id: 1,
+        name: 'Pod akacjami',
+        city: 'Warszawa',
+        rating: 8.3,
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet dolor eu nisi pretium auctor.',
+        image: ''
+      },
+      {
+        id: 2,
+        name: 'Dębowy',
+        city: 'Lublin',
+        rating: 8.8,
+        description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet dolor eu nisi pretium auctor.',
+        image: ''
+      },
+    ]
+ 
+    state = {
+      hotels: [],
+      loading: true,
+    }
+  
+  searchHandler(term) {
+    console.log('szukaj z app', term);
+    const hotels = [...this.hotels]
+      .filter( x => x.name
+      .toLowerCase()
+      .includes(term.toLowerCase()))
+
+    this.setState({hotels})
+  }  
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        hotels:this.hotels, 
+        loading:false})
+    }, 1000)
+  }
 
 
-function App() {
-  return (
-    <div className="App">
-        <Header/>
-        <Menu/>
-        <Hotels/>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+          <Header onSearch={(term) => this.searchHandler(term)}/>
+          <Menu/>
+         
+          {this.state.loading 
+            ? (
+            <LoadingIcon />
+          ) : (
+            <Hotels hotels={this.state.hotels}/>
+          )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Hotels/Hotel/Hotel.js
+++ src/components/Hotels/Hotel/Hotel.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import PropTypes from 'prop-types'
 import styles from './Hotel.module.css'
 import hotelImg from '../../../assets/images/hotel.jpg'
 
-function Hotel() {
+const propTypes = {
+	name: PropTypes.string.isRequired,
+	city: PropTypes.string.isRequired,
+	rating: PropTypes.number.isRequired,
+	description: PropTypes.string.isRequired,
+}
+
+function Hotel(props) {
 	return (
 		<div className={`card ${styles.hotel}`}>
 			<div className='card-body'>
 				<div className='row'>
 					<div className='col-4'>
-						{/*Dołączamy zdjęcie za pomocą zmiennej*/}
 						<img
 							src={hotelImg}
 							alt=''
@@ -18,11 +24,11 @@ function Hotel() {
 					<div className='col-8'>
 						<div className='row'>
 							<div className='col'>
-								<p className={styles.title}>Pensjonat</p>
-								<span className="badge text-bg-light">Warszawa</span>
+								<p className={styles.title}>{props.name}</p>
+								<span className="badge text-bg-light">{props.city}</span>
 							</div>
 							<div className='col text-end'>
-								<h5>Ocena: 8.3</h5>
+								<h5>Ocena: {props.rating}</h5>
                                 <a href='#section' className='btn btn-primary mt-2 px-4 float-end'>
                                 Pokaż
                                 </a>
@@ -30,12 +36,14 @@ function Hotel() {
 						</div>
 					</div>
 					<div className='col-12'>
-						<p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sit amet dolor eu nisi pretium auctor.</p>
+						<p className={styles.description}>{props.description}</p>
 					</div>
 				</div>
 			</div>
 		</div>
 	)
 }
+
+Hotel.propTypes = propTypes
 
 export default Hotel
